fb82b06f5d8439241d7424332bca76e733ce9929
xumengfan <xmf@xkeshi.com>
2017-07-08 12:08:15 +0800

    添加测试文件

---
+++ MFControlLibrary/Classes/PHDView.h
@@ -0,0 +1,18 @@
+//
+//  PHDView.h
+//  Pods
+//
+//  Created by XuMengFan on 2017/7/8.
+//
+//
+
+// ///
+
+#import <UIKit/UIKit.h>
+
+/*
+ 测试用的,知道文件添加到哪里
+ */
+@interface PHDView : UIView
+
+@end
---
+++ MFControlLibrary/Classes/PHDView.m
@@ -0,0 +1,21 @@
+//
+//  PHDView.m
+//  Pods
+//
+//  Created by XuMengFan on 2017/7/8.
+//
+//
+
+#import "PHDView.h"
+
+@implementation PHDView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
